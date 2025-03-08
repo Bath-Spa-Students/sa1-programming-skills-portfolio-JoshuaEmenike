06fcb0cde39a32bbc2ac14c346127dd9cba9763a
JoshuaEmenike <joshuaemenike40@gmail.com>
2025-03-07 23:57:40 -0800

    Simple sums

---
+++ Exercise2.py
@@ -0,0 +1,8 @@
+# Declare variable 1
+jean = 8
+#Declare variable 2
+purple = 10
+#Creating a variable that will serve as the sum of the 1st and 2nd variable
+Honoured= jean + purple
+# Printing the resluts
+print(Honoured)
